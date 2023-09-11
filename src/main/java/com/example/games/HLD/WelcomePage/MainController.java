2eb2ccd4fd515238340a7103ee5b4718f13da3d0
Bishesh99 <bisheshsingh.99@gmail.com>
2023-09-11 14:50:21 +0530

    Directly connecting to backend api instead of any unnecessary dependency

--- src/main/java/com/example/games/HLD/WelcomePage/MainController.java
+++ src/main/java/com/example/games/HLD/WelcomePage/MainController.java
@@ -1,7 +1,8 @@
 package com.example.games.HLD.WelcomePage;
 
 import com.example.games.Constants.Constant;
-import com.example.games.LLD.WelcomePage.Activity.PageUpdate;
+import com.example.games.LLD.WelcomePage.Constants.Attributes;
+import com.example.games.LLD.WelcomePage.Models.API;
 import com.example.games.Models.WebPageController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,11 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController implements WebPageController {
     @Autowired
-    private PageUpdate pageUpdate;
+    private API api;
 
     @GetMapping(value = "/")
     public String welcomePage(Model model) {
-        model = pageUpdate.update(model);
+        model.addAttribute(Attributes.GAME_LIST, api.getGamesList());
 
         return Constant.PageInfos.MAIN_WELCOME_PAGE.getPageName();
     }
